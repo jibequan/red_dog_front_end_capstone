902a3c02750844749ae6a0c3e594fc257c1af89a
Sam Kimball <jibequan@gmail.com>
2018-03-20 10:50:36 -0500

    Added listener on login button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 "use strict";
 
 console.log("Hello main.js");
+
+let $ = require('jquery'),
+    login = require("./user");
+
+$("#login").click(function(){
+  console.log("clicked on Signin");
+  login.logInGoogle()
+  .then((result) => {
+    console.log("result from login", result.user.uid);
+    login.setUser(result.user.uid);
+    $("#login").addClass("is-hidden");
+    // $("#logout").removeClass("is-hidden");
+  });
+});
