34a996fa88a45fa14bbc5ff8f4b415f354986808
Sam Kimball <jibequan@gmail.com>
2018-03-20 14:50:39 -0500

    Added selection behavior

---
+++ js/nav_behavior.js
@@ -0,0 +1,22 @@
+"use strict";
+
+function removeSelected() {
+  let selected = document.getElementsByClassName('item_selected');
+  for (var i = 0; i < selected.length; i++) {
+    selected[i].classList.replace("item_selected", "lights");
+  }
+}
+
+function navSelected(e) {
+    removeSelected();
+    let parentDiv = e.target.parentNode;
+    if (parentDiv.classList.contains("lights") == true) {
+      parentDiv.classList.replace("lights", "item_selected");
+    }else if (parentDiv.classList.contains("lights") == false) {
+      let lights = parentDiv.querySelector(".lights");
+      lights.classList.replace("lights", "item_selected");
+    }
+}
+
+
+module.exports = {navSelected};
