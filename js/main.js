406e5a694bd50bb42ec2c8b562f96a3d8ed59430
Sam Kimball <jibequan@gmail.com>
2018-03-20 14:51:32 -0500

    Linked behavior for selected items

--- js/main.js
+++ js/main.js
@@ -3,15 +3,24 @@
 console.log("Hello main.js");
 
 let $ = require('jquery'),
-    login = require("./user");
+    login = require("./user"),
+    blackout= require("./nav_behavior");
 
-$("#login").click(function(){
-  console.log("clicked on Signin");
-  login.logInGoogle()
-  .then((result) => {
-    console.log("result from login", result.user.uid);
-    login.setUser(result.user.uid);
-    $("#login").addClass("is-hidden");
-    // $("#logout").removeClass("is-hidden");
-  });
+
+let main_area = document.getElementById("main_content");
+let nav = document.getElementById("nav_list");
+
+// $("#login").click(function(){
+//   console.log("clicked on Signin");
+//   login.logInGoogle()
+//   .then((result) => {
+//     console.log("result from login", result.user.uid);
+//     login.setUser(result.user.uid);
+//     $("#login").addClass("is-hidden");
+//     $("#logout").removeClass("is-hidden");
+//   });
+// });
+
+nav.addEventListener("click", (e) => {
+  blackout.navSelected(e);
 });
