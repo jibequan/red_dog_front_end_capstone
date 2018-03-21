e5599a22034d69b96b7eeec7b65ca8d51e49f82c
Sam Kimball <jibequan@gmail.com>
2018-03-21 15:12:54 -0500

    Added call for bike data

---
+++ js/db-interaction.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let $ = require('jquery'),
+    firebase = require("./fb-config");
+
+console.log("Hello db-interaction");
+
+function getBikes(user) {
+  return $.ajax({
+    url: `${firebase.getFBsettings().databaseURL}/bikes.json?orderBy="uid"&equalTo="${user}"`
+  }).done((bikeData) => {
+    return bikeData;
+  });
+}
+
+module.exports = {};
