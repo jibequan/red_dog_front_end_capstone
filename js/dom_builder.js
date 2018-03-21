36159d72c1c5995c1723ac1dc0ceffcb0006dcfc
Sam Kimball <jibequan@gmail.com>
2018-03-21 15:14:55 -0500

    Moving code to other modules

--- js/dom_builder.js
+++ js/dom_builder.js
@@ -1,47 +1,20 @@
 "use strict";
 
 let $ = require('jquery'),
-    loginUser = require("./user");
+    db = require("./db-interaction"),
+    template = require("./display_template"),
+    login = require ("./user");
 
-let showContent = {};
 
-let login = document.getElementById("login");
-let main_content = document.getElementById("main_content");
+function loadBikes() {
+  //Get ID from currently logged in user
+  let currentUser = login.getUser();
+  //Go to the database via promise and get the bikes for that user
+  db.getBikes(currentUser)
+  //Once the data is back, format it and make it ready for the DOM
+  .then((bikeData) => {
+    template.makeBikeGrid(bikeData);
+  })
+}
 
-showContent.showLogin = function() {
-  main_content.innerHTML = `<h3>Login to your account<h3>
-  <p>See your bike repair history, request service, or request a roadside rescue.</p>
-  <button type="button" class="btn btn-dark" id="googLogin">Sign In with Google</button>`;
-  $("#googLogin").click(function(){
-    console.log("clicked on Signin");
-    loginUser.logInGoogle()
-    .then((result) => {
-      console.log("result from login", result.user.uid);
-      login.setUser(result.user.uid);
-      // $("#login").addClass("is-hidden");
-      // $("#logout").removeClass("is-hidden");
-    });
-  });
-};
-
-showContent.showBikes = function() {
-  main_content.innerHTML = `<h3>This is where bikes for sale will show<h3>
-  <p>This will show a gallery of bikes for sale.</p>`;
-};
-
-showContent.showParts = function() {
-  main_content.innerHTML = `<h3>This is where user will be able to look for parts.<h3>
-  <p>My hope is that this will have a pretty robust search.</p>`;
-};
-
-showContent.showService = function() {
-  main_content.innerHTML = `<h3>This is where the user's bikes will show<h3>
-  <p>There will be general information about the bikes and the ability to see past repairs.</p>`;
-};
-
-showContent.showRescue = function() {
-  main_content.innerHTML = `<h3>This is where the user will be able to request a roadside rescue. <h3>
-  <p>The plan is to tap into the mobile devices location.</p>`;
-};
-
-module.exports = {showContent};
+module.exports = {};
