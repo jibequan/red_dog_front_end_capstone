df659b7aafc5b7a19c79033c1746c71216fa351e
Sam Kimball <jibequan@gmail.com>
2018-03-21 20:05:46 -0500

    Basic shell for function

--- js/display_template.js
+++ js/display_template.js
@@ -1,7 +1,9 @@
 "use strict";
 
-function makeBikeGrid() {
+let main_area = document.getElementById("main_content");
 
+function makeBikeGrid() {
+  //This will control how the data coming back from FB is torn apart and added to grid.
 }
 
 module.exports = {makeBikeGrid};
