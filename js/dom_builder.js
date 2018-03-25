7748f281338f71bd32dbe4ebc57c2b7b28f18a90
Sam Kimball <jibequan@gmail.com>
2018-03-25 17:39:39 -0500

    Added function to start display of DOM content
    Cleaned up references to renamed modules too.

--- js/dom_builder.js
+++ js/dom_builder.js
@@ -1,9 +1,23 @@
 "use strict";
 
-let $ = require('jquery'),
-    db = require("./db-interaction"),
-    template = require("./display_template"),
+let db = require("./db-interaction"),
+    template = require("./user_info"),
     login = require ("./user");
+
+let main_content = document.getElementById("main_content");
+let content = {};
+
+content.showMyBikes = function() {
+  //update with helper functions
+  main_content.innerHTML = `<h3>Here are your bikes.</h3>
+  <p class="subheading">You can additional bikes or request service for one already associated with your account.</p>
+  <button type="button" class="btn btn-dark" id="add_bike">+Add bike</button>
+  <div id="gallery"></div>`;
+  template.makeBikeGrid();
+  let addBike = document.getElementById("add_bike");
+  addBike.addEventListener("click", ()=>{
+  });
+};
 
 
 function loadBikes() {
@@ -17,4 +31,4 @@ function loadBikes() {
   });
 }
 
-module.exports = {};
+module.exports = {content};
