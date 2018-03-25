6545f956fc2d240177fb94f34ca1e1a79c182d3a
Sam Kimball <jibequan@gmail.com>
2018-03-25 16:41:56 -0500

    Moved login behavior to Service
    Also, closed heading tags causing issues with formatting.

--- js/nav_behavior.js
+++ js/nav_behavior.js
@@ -2,7 +2,7 @@
 
 let login = require("./user"),
     $ = require("jquery"),
-    bikeForm = require("./forms");
+    forms = require("./forms");
 
 let loginDiv = document.getElementById("login");
 let main_content = document.getElementById("main_content");
@@ -27,49 +27,65 @@ function navSelected(e) {
     }
 }
 
-showContent.showLogin = function() {
-  //update with helper functions
-  main_content.innerHTML = `<h3>Login to your account<h3>
-  <p>See your bike repair history, request service, or request a roadside rescue.</p>
-  <button type="button" class="btn btn-dark" id="googLogin">Sign In with Google</button>`;
-  $("#googLogin").click(function(){
-    console.log("clicked on Signin");
-    login.logInGoogle()
-    .then((result) => {
-      console.log("result from login", result.user.uid);
-      login.setUser(result.user.uid);
-      showContent.showService();
-    });
-  });
-};
-
 showContent.showBikes = function() {
   //update with helper functions
-  main_content.innerHTML = `<h3>This is where bikes for sale will show<h3>
+  main_content.innerHTML = `<h3>This is where bikes for sale will show</h3>
   <p>This will show a gallery of bikes for sale.</p>`;
 };
 
 showContent.showParts = function() {
   //update with helper functions
-  main_content.innerHTML = `<h3>This is where user will be able to look for parts.<h3>
+  main_content.innerHTML = `<h3>This is where user will be able to look for parts.</h3>
   <p>My hope is that this will have a pretty robust search.</p>`;
 };
 
 showContent.showService = function() {
   //update with helper functions
-  main_content.innerHTML = `<h3>Here are your bikes.<h3>
-  <p>You can additional bikes or request service for one already associated with your account.</p>
+  main_content.innerHTML = `<h3>Repair and Mod Options</h3>
+  <p class="subheading">Need us to wrench on your bike? If you haven’t had us look at it in the past, give us the lowdown and we can give it a peek.
+  </p>
+  <p>Step 1 -> <span class="disabled">Step 2 -> </span><span class="disabled">Step 3</span></p>
+  <p>Sign in with your Google account and you will be able to track the progress of your repair and revisit past repairs.</p>
+  <button type="button" class="btn btn-dark" id="googLogin">Sign In with Google</button>
+  <p>Not on the best terms with the Googs? Not problem. You can still sign up for service as a guest. We’ve got you either way.
+  </p>
+  <button type="button" class="btn btn-dark" id="guestLogin">Sign In as Guest</button>`;
+  $("#googLogin").click(function(){
+    console.log("clicked on Google Signin");
+    login.logInGoogle()
+    .then((result) => {
+      console.log("result from login", result.user.uid);
+      login.setUser(result.user.uid);
+      showContent.showMyBikes();
+    });
+  });
+  $("#guestLogin").click(function(){
+    console.log("clicked on Guest Signin");
+    forms.showGuestForm();
+    });
+};
+
+showContent.showMyBikes = function() {
+  //update with helper functions
+  main_content.innerHTML = `<h3>Here are your bikes.</h3>
+  <p class="subheading">You can additional bikes or request service for one already associated with your account.</p>
   <button type="button" class="btn btn-dark" id="add_bike">+Add bike</button>`;
   let addBike = document.getElementById("add_bike");
   addBike.addEventListener("click", ()=>{
-    bikeForm.showForm();
+    forms.showGuestForm();
   });
 };
 
 showContent.showRescue = function() {
   //update with helper functions
-  main_content.innerHTML = `<h3>This is where the user will be able to request a roadside rescue. <h3>
+  main_content.innerHTML = `<h3>This is where the user will be able to request a roadside rescue. </h3>
   <p>The plan is to tap into the mobile devices location.</p>`;
+};
+
+showContent.showArmy = function() {
+  //update with helper functions
+  main_content.innerHTML = `<h3>This is where the user will be able to see the Moped Army. </h3>
+  <p>Maybe an API?</p>`;
 };
 
 
