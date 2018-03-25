796791f55ce15fb8369cf2dd5f13676e3b90d4d6
Sam Kimball <jibequan@gmail.com>
2018-03-25 17:38:29 -0500

    Renamed file to user_info

--- js/display_template.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-let main_area = document.getElementById("main_content");
-
-function makeBikeGrid() {
-  //This will control how the data coming back from FB is torn apart and added to grid.
-}
-
-module.exports = {makeBikeGrid};
