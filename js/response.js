f7d4ccfbe6d8007ce2cc6ae34722f01f609c180e
Sam Kimball <jibequan@gmail.com>
2018-03-25 23:49:39 -0500

    Started with success message

---
+++ js/response.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let response= {};
+
+function bikeAdded() {
+  let main_content = document.getElementById("main_content");
+  main_content.innerHTML = `<div class="col-sm-10">
+    <h3>Your bike has been added to your account.</h3>
+    <p class="subheading">Head back to your bikes to request ser
+
+  </div>`;
+}
