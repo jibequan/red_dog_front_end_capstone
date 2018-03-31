1228d4f5c34f8a33367892554507c82147e85705
Sam Kimball <jibequan@gmail.com>
2018-03-31 16:24:45 -0500

    Added response for when bike is removed

--- js/response.js
+++ js/response.js
@@ -6,9 +6,18 @@ let bikeAdded= () => {
   let main_content = document.getElementById("main_content");
   main_content.innerHTML = `<div class="col-sm-10">
     <h3>Your bike has been added to your account.</h3>
-    <p class="subheading">Head back to your bikes to request service for another one of your bikes.
+    <p class="subheading">Head back to your bikes to request service for another one of your bikes.</p>
     <button type="button" class="btn btn-dark" id="show_bikes">Back to My Bikes</button>
 	</div>`;
 };
 
-module.exports = {bikeAdded};
+let bikeRemoved= () => {
+  let main_content = document.getElementById("main_content");
+  main_content.innerHTML = `<div class="col-sm-10">
+    <h3>This bike has been removed from your account.</h3>
+    <p class="subheading">Head back to your bikes to request service for another one of your bikes or to edit your bikes.</p>
+    <button type="button" class="btn btn-dark" id="show_bikes">Back to My Bikes</button>
+	</div>`;
+};
+
+module.exports = {bikeAdded, bikeRemoved};
