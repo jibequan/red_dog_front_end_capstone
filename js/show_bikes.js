e3b215cb5f4a4c05295f69d31e683d2711fbdf6c
Sam Kimball <jibequan@gmail.com>
2018-06-10 09:25:25 -0500

    Refactor using classes for grid and make easier to read

--- js/show_bikes.js
+++ js/show_bikes.js
@@ -9,30 +9,36 @@ let main_content = document.getElementById("main_content");
 
 function makeBikeGrid(bikes_data) {
   let keys = Object.keys(bikes_data);
-  keys.forEach((item) => {        
-    let bike_card = `<div id="${bikes_data[item].bike_Id}"class="card col-sm-5 justify-content-around">
-                      <img class="card-img-top" src="http://via.placeholder.com/150x100" alt="Photo of your moped/scooter">
-                      <div class="card-body">
-                        <h5 class="card-title">"${bikes_data[item].nickname}" | ${bikes_data[item].year} | ${bikes_data[item].make} | ${bikes_data[item].model}</h5>
-                        <p class="card-text">Repair History</p>
-                        <div class="list-group">
-                          <a href="#" class="list-group-item">02/14/2018: Cleaned carberator, new chain, fixed signal</a>
-                        </div>
-                        <a href="#" class="btn btn-danger delete_bike">Delete</a>
-                        <a href="#" class="btn btn-secondary edit_bike">Edit</a>
-                        <a href="#" class="btn btn-dark service_bike">Request Service</a>
-                      </div>
-                    </div>`;
+  keys.forEach((item) => {
+    let bike_card = `\
+    <div id="${bikes_data[item].bike_Id}" class="card">\
+      <img class="card-img-top" src="http://via.placeholder.com/150x100" alt="Photo of your moped/scooter">\
+      <div class="card-body">\
+        <h5 class="card-title">"${bikes_data[item].nickname}" | ${bikes_data[item].year} | ${bikes_data[item].make} | ${bikes_data[item].model}</h5>\
+        <p class="card-text">Repair History</p>\
+        <div class="list-group">\
+          <a href="#" class="list-group-item">02/14/2018: Cleaned carberator, new chain, fixed signal</a>\
+        </div>\
+        <a href="#" class="btn btn-danger delete_bike">Delete</a>\
+        <a href="#" class="btn btn-secondary edit_bike">Edit</a>\
+        <a href="#" class="btn btn-dark service_bike">Request Service</a>\
+      </div>\
+    </div>`;
     let gallery = document.getElementById("gallery");
     gallery.innerHTML+= bike_card;
   });
 }
 
 function showMyBikes(bikes_data) {
-  main_content.innerHTML = `<h3>Hey ${user.getCompleteUser().name}. Here are your bikes.</h3>
-  <p class="subheading">You can additional bikes or request service for one already associated with your account.</p>
-  <button type="button" class="btn btn-dark" id="add_bike">+ Add bike</button>
-  <div id="gallery" class="row"></div>`;
+  main_content.innerHTML = `\
+  <div class="main__container--service">\
+    <div class="main__header">\
+      <h3 class="main__heading">Hey ${user.getCompleteUser().name}. Here are your bikes.</h3>\
+      <p class="subheading">You can additional bikes or request service for one already associated with your account.</p>\
+      <button type="button" class="btn btn-dark" id="add_bike">+ Add bike</button>\
+    </div>\
+    <div id="gallery" class="main__supporting--service"></div>\
+  </div>`;
   makeBikeGrid(bikes_data);
 }
 
