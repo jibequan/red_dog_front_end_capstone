425e688c737ae27eb1f55b8c09461fa97b3611f5
Sam Kimball <s.j.kimball@icloud.com>
2018-10-16 10:46:47 -0500

    Add method to add content to DOM

--- js/dom_builder.js
+++ js/dom_builder.js
@@ -90,4 +90,9 @@ content.showArmy = () => {
     </div>`;
 };
 
+content.contentToDom = (content) => {
+  let main_content = document.getElementById("main_content");
+  main_content.innerHTML = content;
+};
+
 module.exports = {content};
