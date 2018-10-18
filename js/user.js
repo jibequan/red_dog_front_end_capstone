b5f4a7a87ec7448bd9e919d408568bec33f7e176
Sam Kimball <s.j.kimball@icloud.com>
2018-10-18 13:22:23 -0500

    Remove makeCompleteUser

--- js/user.js
+++ js/user.js
@@ -5,12 +5,6 @@ let currentUser = null,
 		fbID: null
 };
 
-function makeCompleteUser(currentUser) {
-	completeUser.name = currentUser.displayName;
-	completeUser.email = currentUser.email;
-	completeUser.uid = currentUser.uid;
-}
-
 function setUserFbUglyId(fUglyID) {
 	completeUser.fbID = fUglyID;
 }
